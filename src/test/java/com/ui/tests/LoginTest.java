02ef33bcb7f8a0f9a7872348cc30176d200a32f6
Karthik Duruvasula <krthiksantosh@gmail.com>
2026-03-07 23:16:30 +0530

    un commenting Login Test methods

--- src/test/java/com/ui/tests/LoginTest.java
+++ src/test/java/com/ui/tests/LoginTest.java
@@ -10,19 +10,19 @@ import com.ui.pojo.User;
 @Listeners(com.ui.listeners.TestListener.class)
 public class LoginTest extends TestBase{
 
-//	@Test(description = "Verify if valid user is able to login", groups = { "e2e",
-//			"sanity" }, dataProviderClass = com.ui.dataproviders.LoginDataprovider.class, dataProvider = "LoginDataProvider")
-//	public void loginTest(User user) {
-//		assertEquals(homePage.goToLoginPage().doLoginWith(user.getEmailAddress(), user.getPassword()).getUserName(),
-//				"Karthik Duruvasula");
-//	}
-//
-//	@Test(description = "Login test with CSV Test data", groups = {
-//			"sanity" }, dataProviderClass = com.ui.dataproviders.LoginDataprovider.class, dataProvider = "LoginTestCSVDataProvider")
-//	public void loginCSVTest(User user) {
-//		assertEquals(homePage.goToLoginPage().doLoginWith(user.getEmailAddress(), user.getPassword()).getUserName(),
-//				"Karthik Duruvasula");
-//	}
+	@Test(description = "Verify if valid user is able to login", groups = { "e2e",
+			"sanity" }, dataProviderClass = com.ui.dataproviders.LoginDataprovider.class, dataProvider = "LoginDataProvider")
+	public void loginTest(User user) {
+		assertEquals(homePage.goToLoginPage().doLoginWith(user.getEmailAddress(), user.getPassword()).getUserName(),
+				"Karthik Duruvasula");
+	}
+
+	@Test(description = "Login test with CSV Test data", groups = {
+			"sanity" }, dataProviderClass = com.ui.dataproviders.LoginDataprovider.class, dataProvider = "LoginTestCSVDataProvider")
+	public void loginCSVTest(User user) {
+		assertEquals(homePage.goToLoginPage().doLoginWith(user.getEmailAddress(), user.getPassword()).getUserName(),
+				"Karthik Duruvasula");
+	}
 
 	@Test(description = "Login test with Excel Test data", groups = {
 			"sanity" }, dataProviderClass = com.ui.dataproviders.LoginDataprovider.class, dataProvider = "LoginTestExcelDataProvider", retryAnalyzer = com.ui.listeners.MyRetryAnalyzer.class)
